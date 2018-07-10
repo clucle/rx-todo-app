af87df267f90236a9bb5a7a33274b9a4ca61a0d5
clucle <wjdenwls123@gmail.com>
2018-07-10 15:51:38 +0900

    feat: recycler view accepted sample data and item xml

---
+++ app/src/main/java/com/github/clucle/todo_app/view/adapter/TodoListAdapter.java
@@ -0,0 +1,47 @@
+package com.github.clucle.todo_app.view.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.clucle.todo_app.R;
+
+import java.util.ArrayList;
+
+public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.ViewHolder> {
+
+  private ArrayList<String> mDataSet;
+
+  public TodoListAdapter(ArrayList<String> dataSet) {
+    mDataSet = dataSet;
+  }
+
+  @NonNull
+  @Override
+  public TodoListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_todo, parent, false);
+    return new ViewHolder(v);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull TodoListAdapter.ViewHolder holder, int position) {
+    holder.title.setText(mDataSet.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
+    return mDataSet.size();
+  }
+
+  public class ViewHolder extends RecyclerView.ViewHolder {
+    public TextView title;
+
+    public ViewHolder(View itemView) {
+      super(itemView);
+      title = itemView.findViewById(R.id.text_view_title);
+    }
+  }
+}
--- app/src/main/java/com/github/clucle/todo_app/view/fragment/TodoFragment.java
+++ app/src/main/java/com/github/clucle/todo_app/view/fragment/TodoFragment.java
@@ -3,31 +3,74 @@ package com.github.clucle.todo_app.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.github.clucle.todo_app.R;
 import com.github.clucle.todo_app.presenter.TodoPresenter;
+import com.github.clucle.todo_app.view.adapter.TodoListAdapter;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class TodoFragment extends Fragment implements TodoPresenter.View {
 
+  @BindView(R.id.recycler_view_todo)
+  RecyclerView recyclerViewTodo;
+  @BindView(R.id.btn_add_todo)
+  Button btnAddTodo;
+
+  private RecyclerView.LayoutManager mLayoutManager;
+  private RecyclerView.Adapter mAdapter;
+  private ArrayList<String> mDataset;
+
   private Unbinder unbinder;
   private TodoPresenter todoPresenter;
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    todoPresenter = new TodoPresenter(this);
+  }
 
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_todo, container, false);
 
-    todoPresenter = new TodoPresenter(this);
     unbinder = ButterKnife.bind(this, view);
+
+    mDataset = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+      mDataset.add("hi : " + i);
+    }
+    mLayoutManager = new LinearLayoutManager(getContext());
+    mAdapter = new TodoListAdapter(mDataset);
+    recyclerViewTodo.setHasFixedSize(false);
+
+    recyclerViewTodo.setLayoutManager(mLayoutManager);
+    recyclerViewTodo.setAdapter(mAdapter);
+
+    btnAddTodo.setOnClickListener(addTodoListener);
 
     return view;
   }
+
+  View.OnClickListener addTodoListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      mDataset.add("lol");
+      mAdapter.notifyItemInserted(mDataset.size() - 1);
+      mAdapter.notifyDataSetChanged();
+    }
+  };
 
   @Override
   public void onDestroyView() {
