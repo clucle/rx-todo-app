218ffdbad76c715158470c08a32d80cf3df3ee55
clucle <wjdenwls123@gmail.com>
2018-07-26 16:13:09 +0900

    feat: todo 추가버튼을 fab btn으로 바꿈
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- app/src/main/java/com/github/clucle/todo_app/view/fragment/TodoFragment.java
+++ app/src/main/java/com/github/clucle/todo_app/view/fragment/TodoFragment.java
@@ -1,15 +1,15 @@
 package com.github.clucle.todo_app.view.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.github.clucle.todo_app.R;
 import com.github.clucle.todo_app.presenter.TodoPresenter;
@@ -26,12 +26,11 @@ public class TodoFragment extends Fragment implements TodoPresenter.View {
 
   @BindView(R.id.recycler_view_todo)
   RecyclerView recyclerViewTodo;
-  @BindView(R.id.btn_add_todo)
-  Button btnAddTodo;
+  @BindView(R.id.fab_todo_add)
+  FloatingActionButton fabTodoAdd;
 
-  private RecyclerView.LayoutManager mLayoutManager;
   private RecyclerView.Adapter mAdapter;
-  private ArrayList<String> mDataset;
+  private ArrayList<String> mDataSet;
 
   private Unbinder unbinder;
   private TodoPresenter todoPresenter;
@@ -44,17 +43,18 @@ public class TodoFragment extends Fragment implements TodoPresenter.View {
 
   @Nullable
   @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_todo, container, false);
 
     unbinder = ButterKnife.bind(this, view);
 
-    mDataset = new ArrayList<>();
+    mDataSet = new ArrayList<>();
     for (int i = 0; i < 100; i++) {
-      mDataset.add("hi : " + i);
+      mDataSet.add("hi : " + i);
     }
-    mLayoutManager = new LinearLayoutManager(getContext());
-    mAdapter = new TodoListAdapter(mDataset);
+
+    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+    mAdapter = new TodoListAdapter(mDataSet);
     recyclerViewTodo.setHasFixedSize(false);
 
     recyclerViewTodo.setLayoutManager(mLayoutManager);
@@ -64,16 +64,17 @@ public class TodoFragment extends Fragment implements TodoPresenter.View {
     final RecyclerView.ItemDecoration itemDecoration = new VerticalSpaceItemDecoration(itemVerticalSpace);
     recyclerViewTodo.addItemDecoration(itemDecoration);
 
-    btnAddTodo.setOnClickListener(addTodoListener);
+    fabTodoAdd.setOnClickListener(addTodoListener);
 
     return view;
   }
 
+
   View.OnClickListener addTodoListener = new View.OnClickListener() {
     @Override
     public void onClick(View v) {
-      mDataset.add("lol");
-      mAdapter.notifyItemInserted(mDataset.size() - 1);
+      mDataSet.add("lol");
+      mAdapter.notifyItemInserted(mDataSet.size() - 1);
       mAdapter.notifyDataSetChanged();
     }
   };
